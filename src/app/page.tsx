0caee848229d17abb2dd33f7eb65ee848c02f360
padwhen <123895854+padwhen@users.noreply.github.com>
2024-01-06 00:33:10 +0200

    Initial commit -Creating navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,61 @@
-import Image from 'next/image'
+import Link from "next/link";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+
+const perks = [
+  {
+    name: "Exclusive Merchandise Access",
+    Icon: ArrowDownToLine,
+    description: "Gain VIP status and be the first to get your hands on limited-edition K-pop merchandise, from autographed albums to exclusive concert goodies. Enjoy a premier shopping experience with access to items that true K-pop enthusiasts crave."
+  },
+  {
+    name: "Fan Club Rewards Program",
+    Icon: CheckCircle,
+    description: "Join our loyalty program and unlock a world of perks! Earn points with every purchase, participate in exciting challenges, and redeem rewards like discounts, freebies, and even meet-and-greet opportunities with your favorite K-pop stars. It's our way of celebrating your devotion to the K-pop community."
+  },
+  {
+    name: "Customized K-pop Bundles",
+    Icon: Leaf,
+    description: "Personalize your K-pop journey with curated bundles tailored to your bias or favorite groups. From fashion-forward apparel to must-have accessories, our customized packages ensure you receive a unique collection that mirrors your fandom passion. Embrace your individuality while celebrating the best of K-pop culture"
+  },
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <>
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Harmony in Style, Rhythm in Fashion  
+          <span className="text-blue-600"> - Your K-Pop Paradise Awaits!</span>
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">Elevate your K-pop experience with trendsetting merch and exclusive collectibles at our one-stop K-pop haven!
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href='/products' className={buttonVariants()}>Browse Trending</Link>
+          <Button variant='ghost'>Our quality promise &rarr; </Button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </MaxWidthWrapper>
+    <section className="border-t border-gray-200 bg-gray-50">
+      <MaxWidthWrapper className="py-20">
+        <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+          {perks.map((perk) => (
+            <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+              <div className="md:flex-shrink-0 flex justify-center">
+                <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                  {<perk.Icon className="w-1/3 h-1/3" />}
+                </div>
+              </div>
+              <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                <h3 className="text-base font-medium text-gray-900">{perk.name}</h3>
+                <p className="mt-3 text-sm text-muted-foreground">{perk.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </MaxWidthWrapper>
+    </section>
+    </>
   )
 }
---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "Albums",
+        value: "albums" as const,
+        featured: [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: "/nav/ui-kits/mixed.jpg"
+            }
+        ]
+    }
+]
